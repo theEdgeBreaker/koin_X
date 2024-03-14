6a1cd21a154e26caf6f931dddfd2a9bdd54c6cbb
theEdgeBreaker <thesanjana2002@gmail.com>
2024-03-14 07:02:52 +0530

    Created Team.js component

---
+++ src/pages/components/Team.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Image from "next/image";
+
+function Team() {
+  return (
+    <div className="bg-rose-300">
+      <h2>Team</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur. Id consequat adipiscing arcu
+        nibh. Eget mattis in mi integer sit egestas. Proin tempor id pretium
+        quam. Facilisis purus convallis quam augue.
+      </p>
+      <div>
+        <div>
+          <Image />
+          <span>John Smith</span>
+          <span>Designation Here</span>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. In justo rutrum sit sit
+          fermentum ut libero hendrerit id. Tellus sit ornare netus sagittis in
+          nunc convallis mattis maecenas. Tempus arcu leo sociis laoreet nec
+          neque sed pellentesque viverra. Consectetur proin amet ut id facilisi
+          quis consectetur. Tellus gravida ultricies feugiat sed eu egestas
+          dolor est ipsum. Malesuada etiam mi gravida praesent interdu
+        </p>
+      </div>
+      <div>
+        <div>
+          <Image />
+          <span>Elina Williams</span>
+          <span>Designation Here</span>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. In justo rutrum sit sit
+          fermentum ut libero hendrerit id. Tellus sit ornare netus sagittis in
+          nunc convallis mattis maecenas. Tempus arcu leo sociis laoreet nec
+          neque sed pellentesque viverra. Consectetur proin amet ut id facilisi
+          quis consectetur. Tellus gravida ultricies feugiat sed eu egestas
+          dolor est ipsum. Malesuada etiam mi gravida praesent interdu
+        </p>
+      </div>
+      <div>
+        <div>
+          <Image />
+          <span>John Smith</span>
+          <span>Designation Here</span>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. In justo rutrum sit sit
+          fermentum ut libero hendrerit id. Tellus sit ornare netus sagittis in
+          nunc convallis mattis maecenas. Tempus arcu leo sociis laoreet nec
+          neque sed pellentesque viverra. Consectetur proin amet ut id facilisi
+          quis consectetur. Tellus gravida ultricies feugiat sed eu egestas
+          dolor est ipsum. Malesuada etiam mi gravida praesent interdu
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Team;
