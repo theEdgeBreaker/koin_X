a64419c8d776621f9cfc14285b25b10c8d9d2516
theEdgeBreaker <thesanjana2002@gmail.com>
2024-03-14 07:02:02 +0530

    Created Tabs.js component

--- src/pages/components/Tabs.js
+++ src/pages/components/Tabs.js
@@ -3,32 +3,50 @@ import Link from "next/link";
 
 function Tabs() {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link>Overview</Link>
-        </li>
-        <li>
-          <Link>Fundamentals</Link>
-        </li>
-        <li>
-          <Link>News Insights</Link>
-        </li>
-        <li>
-          <Link>Sentiments</Link>
-        </li>
-        <li>
-          <Link>Team</Link>
-        </li>
-        <li>
-          <Link>Technicals</Link>
-        </li>
-        <li>
-          <Link>Tokenomics</Link>
-        </li>
-      </ul>
+    <div
+      className="flex flex-row gap-12 border-b-2 border-gray-300 pb-2
+    ml-12 font-semibold text-lg pt-14"
+      style={{ color: "#3E424A" }}
+    >
+      <div>
+        <Link href="#overview" smooth={true} duration={500}>
+          Overview
+        </Link>
+      </div>
+      <div>
+        <Link href="#overview" smooth={true} duration={500}>
+          Fundamentals
+        </Link>
+      </div>
+      <div>
+        <Link href="#news-insights" smooth={true} duration={500}>
+          News Insights
+        </Link>
+      </div>
+      <div>
+        <Link href="#sentiments" smooth={true} duration={500}>
+          Sentiments
+        </Link>
+      </div>
+      <div>
+        <Link href="#team" smooth={true} duration={500}>
+          Team
+        </Link>
+      </div>
+      <div>
+        <Link href="#" smooth={true} duration={500}>
+          Technicals
+        </Link>
+      </div>
+      <div>
+        <Link href="#tokenomics" smooth={true} duration={500}>
+          Tokenomics
+        </Link>
+      </div>
     </div>
   );
 }
 
 export default Tabs;
+
+// ============================
