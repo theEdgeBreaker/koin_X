3ff1c57a37b2f978db9fbf21c031d41b084b1205
theEdgeBreaker <thesanjana2002@gmail.com>
2024-03-14 06:57:42 +0530

    Imported Components to render

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,20 @@ import { Inter } from "next/font/google";
 import NavBar from "./components/NavBar";
 import CoinPage from "./[coinId]";
 import Tabs from "./components/Tabs";
+import About from "./components/About";
+import Team from "./components/Team";
+import Performance from "./components/Performance";
+import dynamic from "next/dynamic";
+import YouMayKnowCarousel from "./components/YouMayKnowCarousel";
+import TrendingCoinsCarousel from "./components/TrendingCoinsCarousel";
+
+const Sentiment = dynamic(() => import("./components/Sentiment"), {
+  ssr: false,
+});
+
+const Tokenomics = dynamic(() => import("./components/Tokenomics"), {
+  ssr: false,
+});
 
 const inter = Inter({
   subsets: ["latin"],
@@ -15,8 +29,40 @@ export default function Home() {
   return (
     <div className={`${inter.variable} font-sans`}>
       <NavBar />
+
       <CoinPage coinId="bitcoin" />
-      <Tabs />
+
+      <div className="md:w-4/6">
+        <Tabs />
+      </div>
+
+      <div className="md:w-4/6" id="overview">
+        <Performance />
+      </div>
+
+      <div className="md:w-4/6" id="sentiments">
+        <Sentiment />
+      </div>
+
+      <div className="md:w-4/6" id="news-insights">
+        <About />
+      </div>
+
+      <div className="md:w-4/6" id="tokenomics">
+        <Tokenomics />
+      </div>
+
+      <div className="md:w-4/6" id="team">
+        <Team />
+      </div>
+
+      <div>
+        <YouMayKnowCarousel />
+      </div>
+
+      <div>
+        <TrendingCoinsCarousel />
+      </div>
     </div>
   );
 }
