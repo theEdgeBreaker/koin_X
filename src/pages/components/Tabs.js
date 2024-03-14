1e1ca3ac111277ba7a15095c6d514dec9b88d8d8
theEdgeBreaker <thesanjana2002@gmail.com>
2024-03-14 19:23:30 +0530

    Added selected link border styling in Tabs component

--- src/pages/components/Tabs.js
+++ src/pages/components/Tabs.js
@@ -1,46 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 function Tabs() {
+  const [active, setActive] = useState("#overview");
+
+  const handleSetActive = (href) => {
+    setActive(href);
+  };
+
   return (
     <div
-      className="flex flex-row gap-12 border-b-2 border-gray-300 pb-2
-    ml-12 font-semibold text-lg pt-14"
+      className="flex flex-row gap-12 border-b-2 border-gray-300 ml-12 font-semibold text-lg pt-14"
       style={{ color: "#3E424A" }}
     >
       <div>
         <Link href="#overview" smooth={true} duration={500}>
-          Overview
+          <div
+            className={`cursor-pointer ${
+              active === "#overview"
+                ? "border-b-4 text-blue-600 pb-2  border-blue-500"
+                : ""
+            }`}
+            onClick={() => handleSetActive("#overview")}
+          >
+            Overview
+          </div>
         </Link>
       </div>
       <div>
-        <Link href="#overview" smooth={true} duration={500}>
-          Fundamentals
+        <Link href="#fundamentals" smooth={true} duration={500}>
+          <div
+            className={`cursor-pointer ${
+              active === "#fundamentals"
+                ? "border-b-4 text-blue-600 pb-2 border-blue-500"
+                : ""
+            }`}
+            onClick={() => handleSetActive("#fundamentals")}
+          >
+            Fundamentals
+          </div>
         </Link>
       </div>
       <div>
         <Link href="#news-insights" smooth={true} duration={500}>
-          News Insights
+          <div
+            className={`cursor-pointer ${
+              active === "#news-insights"
+                ? "border-b-4 text-blue-600 pb-2 border-blue-500"
+                : ""
+            }`}
+            onClick={() => handleSetActive("#news-insights")}
+          >
+            News Insights
+          </div>
         </Link>
       </div>
       <div>
         <Link href="#sentiments" smooth={true} duration={500}>
-          Sentiments
+          <div
+            className={`cursor-pointer ${
+              active === "#sentiments"
+                ? "border-b-4 text-blue-600 pb-2 border-blue-500"
+                : ""
+            }`}
+            onClick={() => handleSetActive("#sentiments")}
+          >
+            Sentiments
+          </div>
         </Link>
       </div>
       <div>
         <Link href="#team" smooth={true} duration={500}>
-          Team
+          <div
+            className={`cursor-pointer ${
+              active === "#team"
+                ? "border-b-4 text-blue-600 pb-2 border-blue-500"
+                : ""
+            }`}
+            onClick={() => handleSetActive("#team")}
+          >
+            Team
+          </div>
         </Link>
       </div>
       <div>
         <Link href="#" smooth={true} duration={500}>
-          Technicals
+          <div
+            className={`cursor-pointer ${
+              active === "#"
+                ? "border-b-4 text-blue-600 pb-2 border-blue-500"
+                : ""
+            }`}
+            onClick={() => handleSetActive("#")}
+          >
+            Technicals
+          </div>
         </Link>
       </div>
       <div>
         <Link href="#tokenomics" smooth={true} duration={500}>
-          Tokenomics
+          <div
+            className={`cursor-pointer ${
+              active === "#tokenomics"
+                ? "border-b-4 text-blue-600 pb-2 border-blue-500"
+                : ""
+            }`}
+            onClick={() => handleSetActive("#tokenomics")}
+          >
+            Tokenomics
+          </div>
         </Link>
       </div>
     </div>
@@ -48,5 +116,3 @@ function Tabs() {
 }
 
 export default Tabs;
-
-// ============================
