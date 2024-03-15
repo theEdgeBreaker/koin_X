513dbcf7f4400489fe858c76094fe8a6ee67b992
theEdgeBreaker <thesanjana2002@gmail.com>
2024-03-15 14:39:20 +0530

    Improved styling of Sentiment.js Component

--- src/pages/components/Sentiment.js
+++ src/pages/components/Sentiment.js
@@ -21,8 +21,15 @@ const Sentiment = () => {
     plotOptions: {
       bar: {
         horizontal: true,
+        barHeight: "30%",
+        distributed: true,
+        dataLabels: {
+          position: "top",
+        },
+        // barWidth: "100%", // Adjust the width of the bars
       },
     },
+
     xaxis: {
       categories: ["buy", "hold", "sell"],
       labels: {
@@ -30,43 +37,51 @@ const Sentiment = () => {
       },
     },
     style: {
-      fontSize: "60px", // Adjust the font size as needed
+      fontSize: "60px",
     },
-    colors: ["#28a745", "#6c757d", "#dc3545"], // Green, Grey, Red
+    colors: ["#28a745", "#C7C8CE", "#dc3545"],
   };
 
   return (
     <div
       className="border border-gray-200 rounded-md shadow-md
-  bg-pink-300"
+     bg-pink-300 px-7 pt-7 pb-11 ml-12 flex flex-col gap-4"
     >
-      <h2 className="text-3xl font-bold text-customBlue">Sentiment</h2>
+      <h2 className="text-3xl font-bold text-customBlue pt-2">Sentiment</h2>
 
       <div className="flex flex-row gap-2 items-center">
         <span className="text-2xl font-semibold text-customDarkBlue">
           Key Events
         </span>
-        <span className=" bg-gray-500 px-2 text-sm rounded-full text-white ">
+        <span className="bg-gray-500 px-2 text-sm rounded-full text-white">
           i
         </span>
       </div>
 
       <SentimentCarousel />
 
-      <div>
-        <span>Analyst Estimates</span>
-        <span className=" bg-slate-600 px-2 rounded-full text-white ">i</span>
+      <div className="flex flex-row gap-2 items-center pt-5">
+        <span className="text-2xl font-semibold text-customDarkBlue">
+          Analyst Estimates
+        </span>
+        <span className="bg-gray-500 px-2 text-sm rounded-full text-white">
+          i
+        </span>
       </div>
-      <div>
-        <div className=" w-32 bg-green-300 text-green-500 px-7 py-10 rounded-full text-4xl text-center">
+
+      <div className="flex flex-row gap-24 items-center">
+        <div
+          className=" text-4xl bg-green-200 text-green-500 flex items-center
+         px-8 h-36 rounded-full "
+        >
           76%
         </div>
-        <div>
+        <div style={{ width: "60%" }}>
           <ReactApexChart
             options={options}
             series={series}
             type="bar"
-            height={120}
+            height={170}
           />
         </div>
       </div>
@@ -75,5 +90,3 @@ const Sentiment = () => {
 };
 
 export default Sentiment;
-
-// -----------------------------------------
