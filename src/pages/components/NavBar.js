58c35a4e1b9962764d316d3a875708038d3c77cb
theEdgeBreaker <thesanjana2002@gmail.com>
2024-03-19 06:00:53 +0530

    Reduce size of logo for sm screen and update sm screen UI

--- src/pages/components/NavBar.js
+++ src/pages/components/NavBar.js
@@ -28,6 +28,7 @@ export default function NavBar() {
             alt="KoinX Logo"
             width={100}
             height={24}
+            className=" w-20 md:w-28"
           />
         </div>
         <div
@@ -71,7 +72,7 @@ export default function NavBar() {
           style={{ width: "73%" }}
         >
           <button
-            className="absolute top-6 right-4 text-white px-3 py-2 text-xl"
+            className="absolute top-6 right-4 text-white px-3 py-3 text-xl"
             onClick={toggleMenu}
           >
             ✕
