ca88d656693b26316738cf064bd006671c759798
theEdgeBreaker <thesanjana2002@gmail.com>
2024-03-19 05:57:22 +0530

    Make carousel responsive

--- src/pages/components/SentimentCarousel.js
+++ src/pages/components/SentimentCarousel.js
@@ -14,62 +14,62 @@ const SentimentCarousel = () => {
         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui officia deserunt mollit anim id est.`,
+        culpa.`,
     },
     {
       image: "/SentimentLogo2.svg",
-      content1:
-        "Egestas purus viverra accumsan in nisl. Morbi tristique senectus et netus. Elit at imperdiet dui accumsan sit amet.",
+      content1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
 
-      content2: `Maecenas pharetra convallis posuere morbi leo urna molestie at
-        elementum. Sapien et ligula ullamcorper malesuada proin libero nunc.
-        Pretium vulputate sapien nec sagittis aliquam malesuada. Morbi leo
-        urna molestie at elementum eu facilisis. Condimentum vitae sapien
-        pellentesque habitant morbi tristique.`,
+      content2: `Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+      culpa.`,
     },
     {
       image: "/SentimentLogo1.svg",
-      content1:
-        "Egestas purus viverra accumsan in nisl. Morbi tristique senectus et netus. Elit at imperdiet dui accumsan sit amet.",
+      content1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
 
-      content2: `Felis eget velit aliquet sagittis id consectetur. Urna nunc id
-        cursus metus aliquam eleifend mi in. Tincidunt augue interdum velit
-        euismod in pellentesque massa. Tincidunt vitae semper quis lectus
-        nulla at volutpat. Sem integer vitae justo eget magna fermentum.
-        Velit aliquet sagittis id consectetur.`,
+      content2: `Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+      culpa.`,
     },
     {
       image: "/SentimentLogo2.svg",
-      content1:
-        "Egestas purus viverra accumsan in nisl. Morbi tristique senectus et netus. Elit at imperdiet dui accumsan sit amet.",
+      content1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
 
-      content2: `Quis hendrerit dolor magna eget est lorem ipsum dolor. Ut diam quam
-        nulla porttitor massa id neque aliquam vestibulum. Sagittis nisl
-        rhoncus mattis rhoncus. Eu scelerisque felis imperdiet proin
-        fermentum leo. Posuere ac ut consequat semper viverra nam libero
-        justo.`,
+      content2: `Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+      culpa.`,
     },
     {
       image: "/SentimentLogo1.svg",
-      content1:
-        "Egestas purus viverra accumsan in nisl. Morbi tristique senectus et netus. Elit at imperdiet dui accumsan sit amet.",
+      content1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
 
-      content2: `Felis eget velit aliquet sagittis id consectetur. Urna nunc id
-        cursus metus aliquam eleifend mi in. Tincidunt augue interdum velit
-        euismod in pellentesque massa. Tincidunt vitae semper quis lectus
-        nulla at volutpat. Sem integer vitae justo eget magna fermentum.
-        Velit aliquet sagittis id consectetur.`,
+      content2: `Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+      culpa.`,
     },
     {
       image: "/SentimentLogo2.svg",
-      content1:
-        "Egestas purus viverra accumsan in nisl. Morbi tristique senectus et netus. Elit at imperdiet dui accumsan sit amet.",
+      content1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
 
-      content2: `Maecenas pharetra convallis posuere morbi leo urna molestie at
-        elementum. Sapien et ligula ullamcorper malesuada proin libero nunc.
-        Pretium vulputate sapien nec sagittis aliquam malesuada. Morbi leo
-        urna molestie at elementum eu facilisis. Condimentum vitae sapien
-        pellentesque habitant morbi tristique.`,
+      content2: `Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+      culpa.`,
     },
   ];
 
@@ -82,29 +82,45 @@ const SentimentCarousel = () => {
   };
 
   return (
-    <div className="relative overflow-hidden">
+    <div className="relative overflow-x-auto md:overflow-hidden ">
+      <style jsx>{`
+        ::-webkit-scrollbar {
+          display: none;
+        }
+        /* Hide scrollbar for Chrome, Safari and Opera */
+        ::-webkit-scrollbar {
+          display: none;
+        }
+        /* Hide scrollbar for IE, Edge and Firefox */
+        -ms-overflow-style: none; /* IE and Edge */
+        scrollbar-width: none; /* Firefox */
+      `}</style>
       <div
-        className="flex transition-transform duration-300"
+        className="flex transition-transform duration-300 "
         style={{
           transform: `translateX(-${currentCard * 60}%)`,
         }}
       >
         {cards.map((card, index) => (
-          <div key={index} className="w-2/3 pr-4 flex-shrink-0 ">
-            <div className=" rounded-md p-6 pb-10 bg-skyBlueMist">
+          <div
+            key={index}
+            className=" w-11/12 md:w-5/6 lg:w-2/3 pr-4 flex-shrink-0 "
+          >
+            <div className=" rounded-md p-2 md:p-6 pb-5 md:pb-10 bg-skyBlueMist">
               <div className="flex flex-row gap-3 ">
                 <Image
                   src={card.image}
                   alt="sentiment Logo"
-                  width={100}
+                  width={80}
                   height={24}
+                  className="w-8"
                 />
-                <p className="text-shadowyNavy font-semibold text-lg">
+                <p className="text-shadowyNavy font-semibold text-xs md:text-lg">
                   {card.content1}
                 </p>
               </div>
 
-              <div className="text-gray-500 text-lg pl-16 pt-3">
+              <div className="text-gray-500 text-xs md:text-lg pl-11 md:pl-16 pt-3">
                 {card.content2}
               </div>
             </div>
@@ -112,7 +128,7 @@ const SentimentCarousel = () => {
         ))}
       </div>
       <button
-        className="absolute left-2 top-1/2 transform -translate-y-1/2   
+        className=" hidden md:block lg:block absolute left-2 top-1/2 transform -translate-y-1/2   
           text-slateBlueGray bg-white px-4 py-2 shadow-md rounded-full 
           z-10 text-2xl font-semibold "
         onClick={moveLeft}
@@ -120,7 +136,7 @@ const SentimentCarousel = () => {
         {"<"}
       </button>
       <button
-        className="absolute right-2 top-1/2 transform -translate-y-1/2
+        className=" hidden md:block lg:block absolute right-2 top-1/2 transform -translate-y-1/2
         text-slateBlueGray bg-white px-4 py-2 shadow-md rounded-full 
         z-10 text-2xl font-semibold"
         onClick={moveRight}
