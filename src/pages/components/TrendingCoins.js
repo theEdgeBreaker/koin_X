3c53248ba459733c1c6aea0a0bcf148c7f72e78d
theEdgeBreaker <thesanjana2002@gmail.com>
2024-03-19 06:04:07 +0530

    Update UI

--- src/pages/components/TrendingCoins.js
+++ src/pages/components/TrendingCoins.js
@@ -23,11 +23,13 @@ export default function TrendingCoins() {
 
   return (
     <div
-      className="border border-gray-200 bg-white rounded-md px-5 py-7 
+      className="border border-gray-200 bg-white md:rounded-md px-5 py-5 pb-7 md:py-7 
       md:ml-6 md:mr-6 lg:ml-5 lg:mr-12 shadow-md"
-      // style={{ width: "27%" }}
     >
-      <h3 className="pb-2 text-3xl font-bold" style={{ color: "#0F1629" }}>
+      <h3
+        className="pb-2 text-xl lg:text-3xl font-bold"
+        style={{ color: "#0F1629" }}
+      >
         Trending Coins (24H)
       </h3>
       {trendingCoins.map((coin) => (
@@ -36,16 +38,18 @@ export default function TrendingCoins() {
           className="flex items-center justify-between pt-5"
         >
           <div
-            className="flex flex-row font-semibold gap-1"
+            className="flex flex-row font-semibold"
             style={{ color: "#0F1629" }}
           >
             <img
               src={coin.item.thumb}
               alt={coin.item.name}
-              className="w-8 h-8 mr-2"
+              className=" w-6 h-6 md:w-8 md:h-8 mr-2"
             />
-            <span className="text-lg">{coin.item.name}</span>
-            <span className="text-lg">({coin.item.symbol.toUpperCase()})</span>
+            <span className=" text-sm lg:text-lg">{coin.item.name}</span>
+            <span className=" text-sm lg:text-lg">
+              ({coin.item.symbol.toUpperCase()})
+            </span>
           </div>
 
           <div
@@ -56,7 +60,7 @@ export default function TrendingCoins() {
             }`}
           >
             <span
-              className={`px-2 py-1 rounded-sm text-lg ${
+              className={`px-2 py-1 rounded-sm text-xs lg:text-lg ${
                 coin.item.data.price_change_percentage_24h.usd > 0
                   ? "text-green-700 bg-green-100"
                   : "text-red-600 bg-red-100"
@@ -67,7 +71,6 @@ export default function TrendingCoins() {
               ) : (
                 <span className="text-xs">&#9660; </span>
               )}
-              {/* {Math.round(coin.item.data.price_change_percentage_24h.usd)} USD */}
               {Math.round(
                 coin.item.data.price_change_percentage_24h.usd * 100
               ) / 100}
